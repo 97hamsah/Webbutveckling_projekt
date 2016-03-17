a64bd1f539beb7d6e7ec6fdd3f35a3e53464dda5
97hamsah <hampusssahlin@gmail.com>
2016-03-17 12:24:08 +0100

    Navbar
    Försöker fixa en fixed navbar som dyker fram när man scrollar.

---
+++ js/js.js
@@ -0,0 +1,14 @@
+$(function(){
+        // Check the initial Poistion of the Sticky Header
+        var stickyHeaderTop = $('.navbar').offset().top;
+
+        $(window).scroll(function(){
+                if( $(window).scrollTop() > stickyHeaderTop ) {
+                        $('.navbar').css({position: 'fixed', top: '0px'});
+                        $('#stickyalias').css('display', 'block');
+                } else {
+                        $('.navbar').css({position: 'static', top: '0px'});
+                        $('#stickyalias').css('display', 'none');
+                }
+        });
+  });
